9e08dc5c4250f82f9ad794f289c624a1f0423dad
siiri <siiri.roschier@icloud.com>
2022-08-19 10:51:32 +0300

    o

---
+++ hw2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 2;
+        if (a == b)
+            System.out.println(a * a);
+        else
+            System.out.println(a + b);
+
+    }
+}
