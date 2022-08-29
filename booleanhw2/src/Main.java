ffce303434641c54b0dee7bc6ad8f72fa69bdc47
siiri <siiri.roschier@icloud.com>
2022-08-29 14:40:17 +0300

    homework

---
+++ booleanhw2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean shining = false;
+        int time = 8;
+        if (shining && (time < 17 && time > 9))
+            System.out.println("Please use sunscreen.");
+    }
+}
