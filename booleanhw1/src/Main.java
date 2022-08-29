5008b63a590a57c034337109c635ed975d031d05
siiri <siiri.roschier@icloud.com>
2022-08-29 14:28:07 +0300

    homework

---
+++ booleanhw1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean weekday = false;
+        boolean vacation = false;
+        if (!weekday || vacation)
+        System.out.println("james sleeps late");
+        else
+        System.out.println("james doesn't sleep late");
+
+    }
+}
