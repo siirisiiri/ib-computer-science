a1cc7c1e900b085232b2e525fc3a0d723a69e468
siiri <siiri.roschier@icloud.com>
2022-08-29 14:42:39 +0300

    homework

---
+++ booleanhw3/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int A = 3;
+        int B = 14;
+        boolean result = false;
+        if ((A - B)%10 == 0)
+            result = true;
+        System.out.println(result);
+    }
+}
