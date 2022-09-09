c2369bf12796bac733684a93f4dfb697b4f7be24
siiri <siiri.roschier@icloud.com>
2022-09-09 11:09:50 +0300

    homework

---
+++ procedurehw5/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] test1 = {1, 2, 3, 4, 5, 6, 4, 3, 2, 1};
+        int x = 1;
+        int [] test2 = {6, 6, 5, 6, 6, 6, 6, 6, 6, 6};
+        for (int v : REMOVE (test1, x, test2))
+            System.out.println(v);
+    }
+    static int[] REMOVE (int [] I, int X, int [] OUT){
+        int k = 0;
+        for (int i = 0; i < 100; i++){
+            if (i > I.length - 1){
+                for (int s = i - k; s < i; s++)
+                    OUT [s] = 0;
+                break;
+            }
+            else if (I [i] == X){
+                k++;
+            }
+            else {
+                OUT [i - k] = I [i];
+            }
+        }
+        return OUT;
+    }
+}
